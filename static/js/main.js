fde04f9e4d5d2d8a7bbd933de694e430ec0107bf
trunghieumickey <trunghieumickey@outlook.com.vn>
2022-07-13 20:29:52 +0700

    Making progress

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
-fetch('./data/data.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+fetch('data/data.json').then(response=>response.json()).then(data=>{
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        let li = document.createElement('li');
+        li.innerHTML = `${item.event_name}`;
+        document.getElementById('root').appendChild(li);
+    }
+}
+)

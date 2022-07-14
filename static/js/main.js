e36845d794c09748b21b1ca382e01a5627b27b10
Lê Trần Trung Hiếu <trunghieumickey@outlook.com.vn>
2022-07-14 14:48:57 +0700

    Making progress

--- static/js/main.js
+++ static/js/main.js
@@ -1,73 +1,84 @@
 function countdownclock(element, timestamp) {
-    var diff = timestamp - (new Date().getTime() + new Date().getTimezoneOffset() * 60000);
-    if (diff < 0) window.location.reload();
-    var seconds = Math.floor(diff / 1000)% 60;
-    var minutes = Math.floor(diff / 60000)% 60;
-    var hours = Math.floor(diff / 3600000)% 24;
+    var diff = timestamp - new Date().getTime();
+    if (diff < 0)
+        window.location.reload();
+    var seconds = Math.floor(diff / 1000) % 60;
+    var minutes = Math.floor(diff / 60000) % 60;
+    var hours = Math.floor(diff / 3600000) % 24;
     var days = Math.floor(diff / 86400000);
+    if (seconds < 10)
+        seconds = "0" + seconds;
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    if (hours < 10)
+        hours = "0" + hours;
     element.textContent = days + " days " + hours + ":" + minutes + ":" + seconds;
 }
 
-fetch('data/data.json').then(response => response.json()).then(data => {
-    var utc = new Date().getTime() + new Date().getTimezoneOffset() * 60000;
+fetch('data/data.json').then(response=>response.json()).then(data=>{
+    var utc = new Date().getTime();
     for (let i = 0; i < data.length; i++) {
         let item = data[i];
         let div = document.createElement('div');
-        let end_time = new Date(item["end_time"]).getTime();
-        let start_time = new Date(item["start_time"]).getTime();
-        if (end_time < utc) continue;
+        let end_time = new Date(item["end_time"]).getTime() - new Date().getTimezoneOffset() * 60000;
+        let start_time = new Date(item["start_time"]).getTime() - new Date().getTimezoneOffset() * 60000;
+        if (end_time < utc)
+            continue;
         if (item["icon_src"] == "iblock") {
             div.className = "countdown-card iblock";
             div.innerHTML = item["event_name"];
-        }
-        else {
+        } else {
             let countdown = document.createElement('div');
             let subtitle = document.createElement('div');
+            let date = document.createElement('div');
+            let info = document.createElement('div');
+            let title = document.createElement('div');
             if (start_time < utc) {
                 div.className = "countdown-card active";
                 subtitle.className = "alert";
                 subtitle.innerHTML = "--In Progress--";
                 countdown.appendChild(subtitle);
-            }
-            else {
+            } else {
                 div.className = "countdown-card";
                 subtitle.className = "subtitle";
                 subtitle.innerHTML = "Starts in";
                 let start_time_div = document.createElement('div');
-                setInterval(countdownclock(start_time_div, start_time));
+                setInterval(countdownclock, 1000, start_time_div, start_time);
                 countdown.appendChild(subtitle);
-                countdown.appendChild(start_time_div);  
+                countdown.appendChild(start_time_div);
             }
+
+            let img = document.createElement('img')
             let icon = document.createElement('a');
             icon.className = "icon";
             icon.href = item["link"];
-            let img = document.createElement('img')
             img.src = "icons/" + item["icon_src"] + ".webp";
             img.alt = item["icon_src"];
-            icon.appendChild(img);
-            div.appendChild(icon);
-            let info = document.createElement('div');
-            
-            let title = document.createElement('div');
             title.className = "title";
             title.innerHTML = item["event_name"];
-            info.appendChild(title);
+            countdown.className = "countdown";
+            info.className = "info";
+            date.className = "date";
+            date.appendChild(document.createTextNode("Start Date: " + new Date(start_time).toLocaleString()));
             if (start_time != end_time) {
                 subtitle = document.createElement('div');
                 subtitle.className = "subtitle";
                 subtitle.innerHTML = "Ends in";
                 let end_time_div = document.createElement('div');
-                setInterval(countdownclock(end_time_div, end_time));
+                setInterval(countdownclock, 1000, end_time_div, end_time);
                 countdown.appendChild(subtitle);
                 countdown.appendChild(end_time_div);
+                date.appendChild(document.createElement('br'));
+                date.appendChild(document.createTextNode("End Date: " + new Date(start_time).toLocaleString()));
             }
-            countdown.className = "countdown";
+            icon.appendChild(img);
+            div.appendChild(icon);
+            info.appendChild(title);
             info.appendChild(countdown);
-            info.className = "info";
+            info.appendChild(date);
             div.appendChild(info);
         }
         document.getElementById("root").appendChild(div);
     }
 }
 )
-

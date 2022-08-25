0c0bebabb475c18d32b766d45216e5a9ce27626a
trunghieumickey <trunghieumickey@outlook.com.vn>
2022-08-26 00:27:19 +0700

    Fix PHP redirect

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php if($_SERVER["HTTPS"]!="on"){header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);exit;}include_once("index.html"); ?>
+<?php if(!isset($_SERVER["HTTPS"])||$_SERVER["HTTPS"]!="on"){header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"],true,301);exit;}include_once("index.html"); ?>
